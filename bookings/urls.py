0f92f1a802bbe25bba1184e60bfab5b53befe406
navneet yadav <105013664+navnt00@users.noreply.github.com>
2025-04-01 21:32:07 +0530

    GET method for getting booked event is done

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import book_event
+from .views import book_event , bookings
 urlpatterns = [
-    path('book_event/' , book_event , name='book_event')
+    path('book_event/' , book_event , name='book_event'),
+    path('bookings/' , bookings , name='bookings'),
+    path('bookings/<str:event_location>/' , bookings , name='booking_by_location'),
 ]
 
--- bookings/views.py
+++ bookings/views.py
@@ -6,15 +6,16 @@ from .models import  Booking
 from events.models import Events
 
 
-
 @api_view(['POST'])
 def book_event(request):
     email = request.data.get('email')
     event_name = request.data.get('event_name')
+    event_date = request.data.get('event_date')
+    event_location = request.data.get('event_location')
     num_tickets = int(request.data.get('num_tickets'))
 
     try:
-        event = Events.objects.filter(event_name = event_name)
+        event = Events.objects.filter(event_name = event_name , event_date = event_date , event_location = event_location)
         if not event.exists():
             return Response('Event not found')
         if event.count() > 1:
@@ -26,7 +27,7 @@ def book_event(request):
     
     total_price = event.event_price * num_tickets
     if event.event_capacity < num_tickets:
-        return Response('Ticket not available')
+        return Response('Sorry you are late Ticket not available')
     
     try:
         user = Users.objects.get(email=email)
@@ -41,4 +42,34 @@ def book_event(request):
     )
     event.event_capacity -= num_tickets
     event.save()
-    return Response('Booking Successfull')
+    return Response({
+        'message' : "Booking Confirm",
+        'booking_details' : {
+            'user' : user.email,
+            'user_name' : f'{user.first_name} {user.last_name}',
+            'event_name' : event.event_name,
+            'event_location' : event.event_location,
+            'event_date' : event.event_date,
+            'num_tickets' : num_tickets,
+            'total_price' : total_price,
+            'event_time' : event.event_time
+        }
+        }, status=201)
+
+@api_view(['GET'])
+def bookings(request):
+    bookings = Booking.objects.all()
+    booked_event = [] 
+    for booking in bookings:
+        booked_event.append({
+            'booking_id' : booking.id,
+            'user' : booking.user.email,
+            'event_name' : booking.events.event_name,
+            'event_location' : booking.events.event_location,
+            'event_date' : booking.events.event_date,
+            'event_time' : booking.events.event_time,
+            'num_tickets': booking.num_tickets,
+            'total_price' : booking.total_price
+        }) 
+    return Response(booked_event, status=200)
+          
